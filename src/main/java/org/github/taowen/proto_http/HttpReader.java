9847b2ba2a092e247b618e6edabc7c9db54515b9
Tao Wen <taowen@gmail.com>
2014-10-13 23:25:33 +0800

    read bytes as stream

--- src/main/java/org/github/taowen/proto_http/HttpReader.java
+++ src/main/java/org/github/taowen/proto_http/HttpReader.java
@@ -10,23 +10,31 @@ public class HttpReader extends Task {
 
     private Iterator<ByteBuffer> byteBufferStream;
     protected String fieldStringValue;
+    private ByteBuffer currentByteBuffer;
 
     public void byteBufferStream(Iterator<ByteBuffer> byteBufferStream) {
         this.byteBufferStream = byteBufferStream;
+        currentByteBuffer = byteBufferStream.next();
     }
 
     @Override
     public void execute() throws Pausable, Exception {
-        ByteBuffer byteBuffer = byteBufferStream.next();
-        byte b = byteBuffer.get();
+        byte b = get();
         if ('G' == b) {
-            assert 'E' == byteBuffer.get();
-            assert 'T' == byteBuffer.get();
+            assert 'E' == get();
+            assert 'T' == get();
             fieldStringValue = "GET";
             pass();
         } else {
             throw new UnsupportedOperationException("not implemented");
         }
+    }
+
+    private byte get() {
+        if (!currentByteBuffer.hasRemaining()) {
+            currentByteBuffer = byteBufferStream.next();
+        }
+        return currentByteBuffer.get();
     }
 
     protected void pass() throws Pausable {
--- src/test/java/org/github/taowen/proto_http/HttpReaderTest.java
+++ src/test/java/org/github/taowen/proto_http/HttpReaderTest.java
@@ -4,13 +4,24 @@ import junit.framework.TestCase;
 
 import java.nio.ByteBuffer;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class HttpReaderTest extends TestCase {
-    public void test() {
+
+    public void testOneByteBuffer() {
         HttpReader reader = createReader("GET");
+        assertThat(reader.readMethod(), is("GET"));
+    }
+
+    public void testTwoByteBuffer() {
+        HttpReader reader = new HttpReader();
+        List<ByteBuffer> byteBuffers = Arrays.asList(
+                ByteBuffer.wrap("G".getBytes()),
+                ByteBuffer.wrap("ET".getBytes()));
+        reader.byteBufferStream(byteBuffers.iterator());
         assertThat(reader.readMethod(), is("GET"));
     }
 
